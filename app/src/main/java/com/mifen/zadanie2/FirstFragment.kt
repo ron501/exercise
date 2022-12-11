12e0092e810350a10d671e285d3f39813392b8ea
ron501 <sergey.sokolov72@yandex.ru>
2022-12-11 21:02:29 +0300

    init 4 commit

--- app/src/main/java/com/mifen/zadanie2/FirstFragment.kt
+++ app/src/main/java/com/mifen/zadanie2/FirstFragment.kt
@@ -6,18 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
+    lateinit var controller: NavController
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
+        super.onViewCreated(view, savedInstanceState)
+        controller = findNavController()
         val button = view.findViewById<Button>(R.id.switchButton)
         button.setOnClickListener {
-            (requireActivity() as MainActivity).switchToSecondFragment()
+       controller.navigate(R.id.action_firstFragment_to_secondFragment)
         }
-        }
-         companion object {
-              @JvmStatic
-        fun newInstance()=
-            FirstFragment()
     }
 }
--- app/src/main/java/com/mifen/zadanie2/SecondFragment.kt
+++ app/src/main/java/com/mifen/zadanie2/SecondFragment.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
+
+    lateinit var controller: NavController
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.switchToFirstButton)
+        controller = findNavController()
+        val button = view.findViewById<Button>(R.id.switchButton)
         button.setOnClickListener {
-            (requireActivity() as MainActivity).switchToFirstFragment()
+            controller.navigateUp()
         }
-    }
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            SecondFragment()
     }
 }
